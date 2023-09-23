8d807913ffcd00db76bd0679979690bfbdee7b7f
yuuu <okazaki@fusic.co.jp>
2023-09-23 16:30:50 +0900

    コードを読むときのスクロール量が大きすぎるのでdescribe間でletは共有しないようにする

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -3,18 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe Report, type: :model do
-  let(:user) do
-    User.create(
-      email: 'user@example.com',
-      name: 'テスト 太郎',
-      password: 'password',
-      password_confirmation: 'password'
-    )
-  end
-  let(:content) { 'コンテンツ' }
-  let(:report) { user.reports.create(title: 'タイトル', content:) }
-
   describe '#editable?' do
+    let(:content) { 'ボウリングのスコア計算プログラムを考え始めました。カレンダーよりもたくさんのコードを書く必要がありそうでワクワクします。' }
+    let(:report) { user.reports.create(title: 'ボウリングのスコア計算プログラムに着手した', content:) }
+
+    let(:user) do
+      User.create(
+        email: 'user@example.com',
+        name: 'テスト 太郎',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+    end
+
     context 'with 自分の日報' do
       subject { report.editable?(user) }
 
@@ -38,6 +39,18 @@ RSpec.describe Report, type: :model do
   end
 
   describe '#created_on' do
+    let(:content) { 'ボウリングのスコア計算プログラムを考え始めました。カレンダーよりもたくさんのコードを書く必要がありそうでワクワクします。' }
+    let(:report) { user.reports.create(title: 'ボウリングのスコア計算プログラムに着手した', content:) }
+
+    let(:user) do
+      User.create(
+        email: 'user@example.com',
+        name: 'テスト 太郎',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+    end
+
     it '日付を返すこと' do
       travel_to("2023-08-20 17:32:46".in_time_zone) do
         expect(report.created_on).to eq "2023-08-20".to_date
@@ -46,22 +59,43 @@ RSpec.describe Report, type: :model do
   end
 
   describe '#save_mentions' do
-    let(:other_report1) { user.reports.create(title: '別の日報1', content: 'コンテンツ') }
-    let(:other_report2) { user.reports.create(title: '別の日報2', content: 'コンテンツ') }
+    let(:report) { user.reports.create(title: 'ボウリングのスコア計算プログラムに着手した', content:) }
 
-    context 'when 別の日報への言及がある' do
+    let(:user) do
+      User.create(
+        email: 'user@example.com',
+        name: 'テスト 太郎',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+    end
+
+    let(:other_report1) { user.reports.create(title: '102日目: ボウリングのスコア計算プログラムの修正', content: <<~CONTENT) }
+      ボウリングのスコア計算のプログラムにメンターの方からコメントをいただいたので修正しました。
+      ダブルストライクのときのスコア計算をするコードに重複があったので、いただいたコメントを参考に修正したところ、重複がなくなり読みやすくなりました。
+    CONTENT
+    let(:other_report2) { user.reports.create(title: 'ボウリングのスコア計算に苦戦している', content: <<~CONTENT) }
+      プログラムを書き始めて1日が経過しましたが、まだボウリングのスコア計算ができていません。
+      ストライクやスペアを考慮せず、スコアを単純に合計することはできています。
+    CONTENT
+
+    context '別の日報への言及がある場合' do
       let(:content) do
         <<~CONTENT
-          別の日報にメンションを送ります。
+          ボウリングのスコア計算プログラムを考え始めました。
+          カレンダーよりもたくさんのコードを書く必要がありそうでワクワクします。
+
+          早速行き詰まってしまったので、他の生徒の方の日報を読ませていただきました。
+
+          ダブルストライクの場合も考慮する必要があるんですね。
           http://localhost:3000/reports/#{other_report1.id}
 
-          もう一つメンションします
+          この方のようにまずはスコアを単純に合計するところからスタートしたいと思いました。
           http://localhost:3000/reports/#{other_report2.id}
         CONTENT
       end
 
       it 'メンションを保存すること' do
-        report.content = content
         report.save
 
         expect(report.mentioning_reports).to match_array([other_report1, other_report2])
@@ -69,10 +103,9 @@ RSpec.describe Report, type: :model do
     end
 
     context 'when 別の日報への言及がない' do
-      let(:content) { '言及のない日報です' }
+      let(:content) { 'ボウリングのスコア計算プログラムを考え始めました。カレンダーよりもたくさんのコードを書く必要がありそうでワクワクします。' }
 
       it 'メンションを保存しないこと' do
-        report.content = content
         report.save
 
         expect(report.mentioning_reports).to be_empty
