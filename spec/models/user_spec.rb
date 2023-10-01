8e29a8b171a97f07dea80e024437c4a811e2c538
yuuu <okazaki@fusic.co.jp>
2023-10-01 16:30:28 +0900

    テストに関わるパラメータはFactoryBotではなくベタ書きする

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,15 +7,15 @@ RSpec.describe User, type: :model do
     subject { user.name_or_email }
 
     context '名前があるとき' do
-      let(:user) { create(:user) }
+      let(:user) { create(:user, name: 'テスト 太郎') }
 
       it { is_expected.to eq 'テスト 太郎' }
     end
 
     context '名前がないとき' do
-      let(:user) { create(:user, name: '') }
+      let(:user) { create(:user, name: '', email: 'alice@example.com') }
 
-      it { is_expected.to eq user.email }
+      it { is_expected.to eq 'alice@example.com' }
     end
   end
 end
